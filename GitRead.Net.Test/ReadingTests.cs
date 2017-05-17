d0462ff7c54407b452da8cc202cf5b05cb278652
Simran Barn <kingsimmy@users.noreply.github.com>
2017-05-17 21:24:28 +0100

    Initial commit

---
+++ GitRead.Net.Test/ReadingTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System.IO;
+using System.IO.Compression;
+using System.Reflection;
+
+namespace GitRead.Net.Test
+{
+    [TestFixture]
+    public class ReadingTests
+    {
+        [Test]
+        public void TestOne()
+        {
+            string repoName = "TestRepo01";
+            string repoDir = Path.GetTempPath() + repoName;
+            if (Directory.Exists(repoDir))
+            {
+                Directory.Delete(repoDir, true);
+            }
+            using (Stream resourceStream = GetType().GetTypeInfo().Assembly.GetManifestResourceStream($"GitRead.Net.Test.Repos.{repoName}.zip"))
+            using(ZipArchive archive = new ZipArchive(resourceStream))
+            {                
+                archive.ExtractToDirectory(repoDir);
+            }
+            Reader myClass = new Reader(repoDir);
+            string res = myClass.ReadLooseFile("d670460b4b4aece5915caf5c68d12f560a9fe3e4");
+            Assert.AreEqual("test content\n", res);
+        }
+    }
+}
---
+++ GitRead.Net/Reader.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.IO;
+using System.IO.Compression;
+
+namespace GitRead.Net
+{
+    public class Reader
+    {
+        private const int whiteSpace = ' ';
+        private const int nullChar = '\0';
+
+        private readonly string repoPath;
+
+        public Reader(string repoPath)
+        {
+            this.repoPath = repoPath;
+        }
+
+        public string ReadLooseFile(string hash)
+        {
+            string folderName = hash.Substring(0, 2);
+            string fileName = hash.Substring(2);
+            FileStream fs = File.OpenRead(Path.Combine(repoPath, ".git", "objects", folderName, fileName));
+            fs.Seek(2, SeekOrigin.Begin);
+            using (StreamReader reader = new StreamReader(new DeflateStream(fs, CompressionMode.Decompress), Encoding.UTF8))
+            {
+                StringBuilder gitFileType = new StringBuilder();
+                int ch;
+                while((ch = reader.Read()) != whiteSpace)
+                {
+                    gitFileType.Append((char)ch);
+                }
+                StringBuilder gitFileSize = new StringBuilder();
+                while ((ch = reader.Read()) != nullChar)
+                {
+                    gitFileSize.Append((char)ch);
+                }
+                string value = reader.ReadToEnd();
+                
+                return value;
+            }
+        }
+    }
+}
