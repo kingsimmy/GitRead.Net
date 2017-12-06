4efdbbc134e79e11096f16c5ff707f34c717d120
Simran Barn <kingsimmy@users.noreply.github.com>
2017-12-06 19:54:14 +0000

    Improve filepaths tests

--- GitRead.Net.Test/RepositoryAnalyzerTests.cs
+++ GitRead.Net.Test/RepositoryAnalyzerTests.cs
@@ -15,14 +15,33 @@ namespace GitRead.Net.Test
             int count = repositoryAnalyzer.GetTotalNumberOfCommits();
             Assert.AreEqual(157, count);
         }
-        
+
         [Test]
-        public void TestGetFilePaths()
+        public void TestGetFilePathsHead()
         {
             string repoDir = TestUtils.ExtractZippedRepo("csharplang.git");
             RepositoryAnalyzer repositoryAnalyzer = new RepositoryAnalyzer(repoDir);
             List<string> filePaths = repositoryAnalyzer.GetFilePaths().ToList();
             Assert.AreEqual(152, filePaths.Count);
+            Assert.True(filePaths.Contains(".gitattributes"));
+            Assert.True(filePaths.Contains(@"meetings\README.md"));
+            Assert.True(filePaths.Contains(@"proposals\rejected\README.md"));
+        }
+
+
+        [Test]
+        public void TestGetFilePathsSpecificCommit()
+        {
+            string repoDir = TestUtils.ExtractZippedRepo("csharplang.git");
+            RepositoryAnalyzer repositoryAnalyzer = new RepositoryAnalyzer(repoDir);
+            List<string> filePaths = repositoryAnalyzer.GetFilePaths("7981ea1fb4d89571fd41e3b75f7c9f7fc178e837").ToList();
+            Assert.AreEqual(6, filePaths.Count);
+            Assert.True(filePaths.Contains("README.md"));
+            Assert.True(filePaths.Contains(@"design-notes\Notes-2016-11-16.md"));
+            Assert.True(filePaths.Contains(@"design-notes\csharp-language-design-notes-2017.md"));
+            Assert.True(filePaths.Contains(@"proposals\async-streams.md"));
+            Assert.True(filePaths.Contains(@"proposals\nullable-reference-types.md"));
+            Assert.True(filePaths.Contains(@"spec\spec.md"));
         }
     }
 }
