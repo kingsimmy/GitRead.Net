86b5821b761c08bd4b832ac25bba8496612affef
Simran Barn <kingsimmy@users.noreply.github.com>
2018-02-18 18:40:32 +0000

    Refactor lines changed functions

---
+++ GitRead.Net/DiffGenerator.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace GitRead.Net
+{
+    internal static class DiffGenerator
+    {
+        internal static (int added, int deleted) GetLinesChanged(string contentBefore, string contentNow)
+        {
+            Dictionary<int, List<string>> allLines1 = contentBefore.Split('\n').GroupBy(x => x.GetHashCode())
+                .ToDictionary(x => x.Key, x => x.ToList());
+            Dictionary<int, List<string>> allLines2 = contentNow.Split('\n').GroupBy(x => x.GetHashCode())
+                .ToDictionary(x => x.Key, x => x.ToList());
+            int added = 0;
+            int deleted = 0;
+            foreach (int hashCode in Enumerable.Concat(allLines1.Keys, allLines2.Keys).Distinct())
+            {
+                bool existed1 = allLines1.TryGetValue(hashCode, out List<string> lines1);
+                bool existed2 = allLines2.TryGetValue(hashCode, out List<string> lines2);
+                if (existed1 && existed2)
+                {
+                    List<string> lines2Copy = new List<string>(lines2);
+                    foreach (string line in lines1)
+                    {
+                        if (lines2Copy.Contains(line))
+                        {
+                            lines2Copy.Remove(line);
+                        }
+                        else
+                        {
+                            deleted++;
+                        }
+                    }
+                    foreach (string line in lines2)
+                    {
+                        if (lines1.Contains(line))
+                        {
+                            lines1.Remove(line);
+                        }
+                        else
+                        {
+                            added++;
+                        }
+                    }
+                }
+                else if (existed1)
+                {
+                    deleted += lines1.Count;
+                }
+                else if (existed2)
+                {
+                    added += lines2.Count;
+                }
+            }
+            return (added, deleted);
+        }
+
+        internal static (int added, int deleted) GetLinesChanged(string contentBefore1, string contentBefore2, string contentNow)
+        {
+            Dictionary<int, List<string>> allLinesBefore1 = contentBefore1.Split('\n').GroupBy(x => x.GetHashCode())
+                .ToDictionary(x => x.Key, x => x.ToList());
+            Dictionary<int, List<string>> allLinesBefore2 = contentBefore2.Split('\n').GroupBy(x => x.GetHashCode())
+                .ToDictionary(x => x.Key, x => x.ToList());
+            Dictionary<int, List<string>> allLinesNow = contentNow.Split('\n').GroupBy(x => x.GetHashCode())
+                .ToDictionary(x => x.Key, x => x.ToList());
+            int added = 0;
+            int deleted = 0;
+            foreach (KeyValuePair<int, List<string>> before in allLinesBefore1)
+            {
+                if (allLinesNow.TryGetValue(before.Key, out List<string> linesNow))
+                {
+                    foreach (string line in before.Value)
+                    {
+                        if (!linesNow.Contains(line))
+                        {
+                            deleted++;
+                        }
+                    }
+                }
+                else
+                {
+                    deleted += before.Value.Count;
+                }
+            }
+            foreach (KeyValuePair<int, List<string>> before in allLinesBefore2)
+            {
+                if (allLinesNow.TryGetValue(before.Key, out List<string> linesNow))
+                {
+                    foreach (string line in before.Value)
+                    {
+                        if (!linesNow.Contains(line))
+                        {
+                            deleted++;
+                        }
+                    }
+                }
+                else
+                {
+                    deleted += before.Value.Count;
+                }
+            }
+            foreach (KeyValuePair<int, List<string>> now in allLinesNow)
+            {
+                List<string> linesBefore1 = allLinesBefore1.ContainsKey(now.Key) ? allLinesBefore1[now.Key] : new List<string>();
+                List<string> linesBefore2 = allLinesBefore2.ContainsKey(now.Key) ? allLinesBefore2[now.Key] : new List<string>();
+                foreach (string line in now.Value)
+                {
+                    if (linesBefore1.Contains(line))
+                    {
+                        linesBefore1.Remove(line);
+                        continue;
+                    }
+                    else if (linesBefore2.Contains(line))
+                    {
+                        linesBefore2.Remove(line);
+                        continue;
+                    }
+                    else
+                    {
+                        added++;
+                    }
+                }
+            }
+            return (added, deleted);
+        }
+    }
+}
--- GitRead.Net/RepositoryAnalyzer.cs
+++ GitRead.Net/RepositoryAnalyzer.cs
@@ -77,8 +77,14 @@ namespace GitRead.Net
                     bool existedInCommitBefore2 = filePathToHashParent2.TryGetValue(filePath, out PathHashMode before2);
                     if ((existedInCommitBefore1 && existedInCommitNow && before1.Hash != now.Hash) && (existedInCommitBefore2 && existedInCommitNow && before2.Hash != now.Hash))
                     {
-                        var changes = GetLinesChanged(before1.Hash, before2.Hash, now.Hash);
-                        modified.Add(new FileChange(now.Path, changes.added + changes.deleted));
+                        int linesAdded = 0;
+                        int linesDeleted = 0;
+                        if (before1.Mode != TreeEntryMode.RegularExecutableFile && before2.Mode != TreeEntryMode.RegularExecutableFile && now.Mode != TreeEntryMode.RegularExecutableFile)
+                        {
+                            (linesAdded, linesDeleted) =
+                                DiffGenerator.GetLinesChanged(repositoryReader.ReadBlob(before1.Hash), repositoryReader.ReadBlob(before2.Hash), repositoryReader.ReadBlob(now.Hash));
+                        }
+                        modified.Add(new FileChange(now.Path, linesAdded + linesDeleted));
                     }
                     else if ((existedInCommitBefore1 || existedInCommitBefore2) && !existedInCommitNow)
                     {
@@ -91,8 +97,13 @@ namespace GitRead.Net
                 }
                 else if(existedInCommitBefore1 && existedInCommitNow && before1.Hash != now.Hash)
                 {
-                    var changes = GetLinesChanged(before1.Hash, before1.Mode, now.Hash, now.Mode);
-                    modified.Add(new FileChange(now.Path, changes.added +  changes.deleted));
+                    int linesAdded = 0;
+                    int linesDeleted = 0;
+                    if (before1.Mode != TreeEntryMode.RegularExecutableFile && now.Mode != TreeEntryMode.RegularExecutableFile)
+                    {
+                        (linesAdded, linesDeleted) = DiffGenerator.GetLinesChanged(repositoryReader.ReadBlob(before1.Hash), repositoryReader.ReadBlob(now.Hash));
+                    }
+                    modified.Add(new FileChange(now.Path, linesAdded + linesDeleted));
                 }
                 else if (existedInCommitBefore1 && !existedInCommitNow)
                 {
@@ -104,129 +115,6 @@ namespace GitRead.Net
                 }
             }
             return new CommitDelta(added, deleted, modified);
-        }
-        
-        private (int added, int deleted) GetLinesChanged(string hash1, TreeEntryMode mode1, string hash2, TreeEntryMode mode2)
-        {
-            if(mode1 == TreeEntryMode.RegularExecutableFile || mode2 == TreeEntryMode.RegularExecutableFile)
-            {
-                return (0, 0);
-            }
-            Dictionary<int, List<string>> allLines1 = repositoryReader.ReadBlob(hash1).Split('\n')
-                .GroupBy(x => x.GetHashCode()).ToDictionary(x => x.Key, x => x.ToList());
-            Dictionary<int, List<string>> allLines2 = repositoryReader.ReadBlob(hash2).Split('\n')
-                .GroupBy(x => x.GetHashCode()).ToDictionary(x => x.Key, x => x.ToList());
-            int added = 0;
-            int deleted = 0;
-            foreach(int hashCode in Enumerable.Concat(allLines1.Keys, allLines2.Keys).Distinct())
-            {
-                bool existed1 = allLines1.TryGetValue(hashCode, out List<string> lines1);
-                bool existed2 = allLines2.TryGetValue(hashCode, out List<string> lines2);
-                if (existed1 && existed2)
-                {
-                    List<string> lines2Copy = new List<string>(lines2);
-                    foreach(string line in lines1)
-                    {
-                        if (lines2Copy.Contains(line))
-                        {
-                            lines2Copy.Remove(line);
-                        }
-                        else
-                        {
-                            deleted++;
-                        }
-                    }
-                    foreach (string line in lines2)
-                    {
-                        if (lines1.Contains(line))
-                        {
-                            lines1.Remove(line);
-                        }
-                        else
-                        {
-                            added++;
-                        }
-                    }
-                }
-                else if (existed1)
-                {
-                    deleted += lines1.Count;
-                }
-                else if (existed2)
-                {
-                    added += lines2.Count;
-                }
-            }
-            return (added, deleted);
-        }
-
-        private (int added, int deleted) GetLinesChanged(string hashBefore1, string hashBefore2, string hashNow)
-        {
-            Dictionary<int, List<string>> allLinesBefore1 = repositoryReader.ReadBlob(hashBefore1).Split('\n')
-                .GroupBy(x => x.GetHashCode()).ToDictionary(x => x.Key, x => x.ToList());
-            Dictionary<int, List<string>> allLinesBefore2 = repositoryReader.ReadBlob(hashBefore2).Split('\n')
-                .GroupBy(x => x.GetHashCode()).ToDictionary(x => x.Key, x => x.ToList());
-            Dictionary<int, List<string>> allLinesNow = repositoryReader.ReadBlob(hashNow).Split('\n')
-                .GroupBy(x => x.GetHashCode()).ToDictionary(x => x.Key, x => x.ToList());
-            int added = 0;
-            int deleted = 0;
-            foreach (KeyValuePair<int, List<string>> before in allLinesBefore1)
-            {
-                if (allLinesNow.TryGetValue(before.Key, out List<string> linesNow))
-                {
-                    foreach(string line in before.Value)
-                    {
-                        if (!linesNow.Contains(line))
-                        {
-                            deleted++;
-                        }
-                    }
-                }
-                else
-                {
-                    deleted += before.Value.Count;
-                }
-            }
-            foreach (KeyValuePair<int, List<string>> before in allLinesBefore2)
-            {
-                if (allLinesNow.TryGetValue(before.Key, out List<string> linesNow))
-                {
-                    foreach (string line in before.Value)
-                    {
-                        if (!linesNow.Contains(line))
-                        {
-                            deleted++;
-                        }
-                    }
-                }
-                else
-                {
-                    deleted += before.Value.Count;
-                }
-            }
-            foreach (KeyValuePair<int, List<string>> now in allLinesNow)
-            {
-                List<string> linesBefore1 = allLinesBefore1.ContainsKey(now.Key) ? allLinesBefore1[now.Key] : new List<string>();
-                List<string> linesBefore2 = allLinesBefore2.ContainsKey(now.Key) ? allLinesBefore2[now.Key] : new List<string>();
-                foreach(string line in now.Value)
-                {
-                    if (linesBefore1.Contains(line))
-                    {
-                        linesBefore1.Remove(line);
-                        continue;
-                    }
-                    else if (linesBefore2.Contains(line))
-                    {
-                        linesBefore2.Remove(line);
-                        continue;
-                    }
-                    else
-                    {
-                        added++;
-                    }
-                }
-            }
-            return (added, deleted);
         }
 
         private int GetLineCount(string hash, TreeEntryMode mode)
